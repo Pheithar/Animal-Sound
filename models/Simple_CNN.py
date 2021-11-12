34cd667911491141b49f1096a6f3a3d0cc4499f7
pheithar <pheithar@hotmail.com>
2021-11-12 12:57:34 +0100

    more automatization, a bit more utils function and possibility of making logs.
    Needs a bit of commentaries, but everything is fine

--- models/Simple_CNN.py
+++ models/Simple_CNN.py
@@ -5,18 +5,24 @@ import torch
 
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.data import DataLoader
 
 from tqdm import tqdm
-
-from torch.utils.data import DataLoader
 
 import torchvision
 import torchvision.transforms as transforms
 
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import accuracy_score
+
+import utils
+
+
 class SimpleCNN(nn.Module):
     # Init
-    def __init__(self, input_channels: int, input_shape: tuple, 
-                 num_labels: int):
+    def __init__(self, input_channels: int, input_shape: tuple,
+                 num_labels: int, net_arch: dict):
 
         super(SimpleCNN, self).__init__()
         # Fields
@@ -25,74 +31,184 @@ class SimpleCNN(nn.Module):
         self.num_labels = num_labels
 
         # Layers - Conv
-        self.conv1 = nn.Conv2d(3, 16, 2, padding="same")
-        self.conv2 = nn.Conv2d(16, 32, 2, padding="same")
-        self.conv3 = nn.Conv2d(32, 64, 2, padding="same")
-        self.conv4 = nn.Conv2d(64, 32, 2, padding="same")
-        self.conv5 = nn.Conv2d(32, 16, 2, padding="same")
-        self.conv6 = nn.Conv2d(16, 8, 2, padding="same")
-        
-        # Layers - Linear
-        self.dense1 = nn.Linear(8*16*4, 64)
-        self.dense2 = nn.Linear(64, num_labels)
+        self.conv = nn.ModuleList()
+
+        for channels, kernel_size in zip(net_arch["conv_channels"],
+                                        net_arch["conv_kernel_size"]):
+            layer = nn.Conv2d(in_channels=input_channels, out_channels=channels,
+                              kernel_size=kernel_size, padding="same")
+            self.conv.append(layer)
+
+            # Update parameters
+            input_channels = channels
 
         # Pooling
-        self.pool = nn.MaxPool2d(2)
+        self.pool = nn.ModuleList()
+
+        for pool_size in net_arch["pooling_size"]:
+            self.pool.append(nn.MaxPool2d(pool_size))
+
+            # Update parameters
+            self.height = int(self.height / pool_size[0])
+            self.width = int(self.width / pool_size[1])
+
+        # Layers - Linear
+        self.dense = nn.ModuleList()
+
+        input_channels = input_channels * self.height * self.width
+
+        for linear_features in net_arch["linear_features"]:
+            layer = nn.Linear(input_channels, linear_features)
+            self.dense.append(layer)
+
+            # Update parameters
+            input_channels = linear_features
+
+        self.dense.append(nn.Linear(input_channels, num_labels))
+
 
         # Activation
         self.relu = nn.ReLU()
-        self.last_activation = nn.Softmax()
+        self.last_activation = eval("nn." + net_arch["last_layer_activation"])
 
         # Flatten
         self.flatten = nn.Flatten()
 
-        # Floaty type
-        self.float()
+    def forward(self, x):
 
-    def forward(self, x_img):
-        x = x_img
-
-        x = self.relu(self.pool(self.conv1(x)))
-        x = self.relu(self.pool(self.conv2(x)))
-        x = self.relu(self.pool(self.conv3(x)))
-        x = self.relu(self.pool(self.conv4(x)))
-        x = self.relu(self.pool(self.conv5(x)))
-
-        x = self.relu(self.pool(self.conv6(x)))
+        for conv, pool in zip(self.conv, self.pool):
+            x = self.relu(pool(conv(x)))
 
         x = self.flatten(x)
 
-        x = self.relu(self.dense1(x))
-        x = self.last_activation(self.dense2(x))
+        for dense in self.dense[:-1]:
+            x = self.relu(dense(x))
+
+        x = self.last_activation(self.dense[-1](x))
 
         return x
 
-    def train_loop(self, num_epochs: int, train_loader: DataLoader, 
+    def fit(self, num_epochs: int, train_loader: DataLoader,
                    validation_loader: DataLoader,
-                   criterion, optimizer):
+                   criterion, optimizer,
+                   show: bool = True, frequency_val : int = 2,
+                   log_file: str = None, plot_file: str = None):
+        train_loss = []
+        train_acc = []
+        plot_epochs_train = []
 
-        self.train()
+        val_loss = []
+        val_acc = []
+        plot_epochs_val = []
+
+        fig, (loss_ax, acc_ax) = plt.subplots(2, 1, figsize=(12, 10))
+
         for epoch in tqdm(range(num_epochs)):
 
             running_loss = 0.0
+            running_accuracy = 0.0
+            # State that the network is in train mode
+            self.train()
             for i, data in enumerate(train_loader):
+
                 # Get the inputs
                 inputs, labels = data.values()
-                
+
+                inputs = utils.get_cuda(inputs)
+                labels = utils.get_cuda(labels)
+
                 # Make them variables
                 optimizer.zero_grad()
 
                 # Forward, backward and around
                 outputs = self(inputs)
-                print(outputs.shape, labels.shape)
+
+                # print(outputs.shape, labels.shape)
+
                 loss = criterion(outputs, labels)
                 loss.backward()
                 optimizer.step()
 
-                running_loss += loss.data.item()
-                if i % 1000 == 999:
-                    print('[%d, %5d] loss: %.3f' %
-                        (epoch + 1, i + 1, running_loss / 1000))
-                    running_loss = 0.0
-        print("Finished training")
+                labels = utils.get_numpy(self.compute_prediction(labels))
+                outputs = utils.get_numpy(self.compute_prediction(outputs))
 
+
+                running_accuracy += accuracy_score(outputs, labels)
+                running_loss += loss.data.item()
+
+
+            train_loss.append(running_loss / len(train_loader))
+            train_acc.append(running_accuracy / len(train_loader))
+            plot_epochs_train.append(epoch+1)
+
+
+            if epoch % frequency_val == 0 or epoch == num_epochs-1:
+                running_loss = 0.0
+                running_accuracy = 0.0
+
+                # State that the network is in validation mode
+                self.eval()
+                for i, data in enumerate(validation_loader):
+
+                    # Get the inputs
+                    inputs, labels = data.values()
+
+                    inputs = utils.get_cuda(inputs)
+                    labels = utils.get_cuda(labels)
+
+                    outputs = self(inputs)
+
+                    loss = criterion(outputs, labels)
+
+                    labels = utils.get_numpy(self.compute_prediction(labels))
+                    outputs = utils.get_numpy(self.compute_prediction(outputs))
+
+                    running_accuracy += accuracy_score(outputs, labels)
+                    running_loss += loss.data.item()
+
+                val_loss.append(running_loss / len(validation_loader))
+                val_acc.append(running_accuracy / len(validation_loader))
+                plot_epochs_val.append(epoch+1)
+
+                if log_file:
+                    with open(log_file, "a") as f:
+                        f.write("-------------------------------------------\n")
+                        f.write(f"Epoch {epoch+1}:\n")
+                        f.write(f"\t Train Loss: {train_loss[-1]:.2f}\n")
+                        f.write(f"\t Train Accuracy: {train_acc[-1]:.2f}\n")
+                        f.write(f"\t Validation Loss: {val_loss[-1]:.2f}\n")
+                        f.write(f"\t Validation Accuracy: {val_acc[-1]:.2f}\n")
+
+
+        loss_ax.set_title("Loss function value in the train and validation sets")
+        loss_ax.plot(plot_epochs_train, train_loss, label="Train Loss")
+        loss_ax.plot(plot_epochs_val, val_loss, label="Validation Loss")
+        loss_ax.set_xlabel("Epochs")
+        loss_ax.set_ylabel("Value")
+        loss_ax.set_xticks(plot_epochs_train)
+        loss_ax.legend()
+
+        acc_ax.set_title("Accuracy of the train and validation sets")
+        acc_ax.plot(plot_epochs_train, train_acc, label="Train Accuracy")
+        acc_ax.plot(plot_epochs_val, val_acc, label="Validation Accuracy")
+        acc_ax.set_xlabel("Epochs")
+        acc_ax.set_ylabel("Percentage")
+        acc_ax.set_xticks(plot_epochs_train)
+        acc_ax.legend()
+
+        if plot_file:
+            plt.savefig(plot_file)
+
+        if show:
+            plt.show()
+        else:
+            plt.close()
+
+        return ((train_loss[-1], train_acc[-1]), (val_loss[-1], val_acc[-1]))
+
+
+    def compute_prediction(self, y):
+        if y.shape[1] == 1:
+            return y.int()
+
+        return torch.max(y, 1)[1]
--- models/utils.py
+++ models/utils.py
@@ -10,15 +10,19 @@ from skimage import io, transform
 
 from sklearn.preprocessing import MultiLabelBinarizer
 
-
 import matplotlib.pyplot as plt
 
+
+### SPECTROGRAM DATASET CLASS ###
 class SpectrogramDataset(Dataset):
     """
     Args:
         csv_path: path to csv file
         root_dir: dir of images
-        path_column: 
+        id_column: column of the id
+        label_column: column of the label
+        transform: transformations of the image
+        one_hot_encode_labels: whether or not to use one hot encoding
     """
     def __init__(self, csv_path: str, root_dir: str, id_column: str, label_column: str, transform=None, one_hot_encode_labels: bool = False):
         self.csv = pd.read_csv(csv_path)
@@ -27,9 +31,9 @@ class SpectrogramDataset(Dataset):
         self.root_dir = root_dir
 
         self.transform = transform
-        
+
         self.one_hot_encode_bool = one_hot_encode_labels
-        
+
         # Giving a class id for each unique class in dataset
         self.lexicon = {class_name: class_id for class_id, class_name in enumerate(self.csv["class"].unique())}
 
@@ -37,15 +41,14 @@ class SpectrogramDataset(Dataset):
             # Assigning label to each row
             labels = list(map(self.lexicon.get, self.csv[label_column]))
             self.labels = np.eye(len(self.lexicon), dtype="float")[labels]
-        
-    
+
+
     def __len__(self):
         return len(self.csv)
 
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-        
+    def __getitem__(self, idx: int):
+        """Return image and label"""
+
         img_name = os.path.join(self.root_dir,
             str(self.csv.iloc[idx, self.col_path_idx]) + ".jpg")
         image = io.imread(img_name)
@@ -54,13 +57,74 @@ class SpectrogramDataset(Dataset):
         if self.one_hot_encode_bool:
             label = self.labels[idx]
         else:
-            label = self.lexicon[self.csv.iloc[idx, self.col_label_idx]]
+            label = [self.lexicon[self.csv.iloc[idx, self.col_label_idx]]]
 
         if self.transform: image = self.transform(image)
-        if type(label) != torch.Tensor:
-            label = torch.from_numpy(label)
 
-        sample = {"image": image, "label": label}
+        sample = {"image": image, "label": np.float32(label)}
 
 
         return sample
+
+    def get_image(self, idx: int):
+        """Return image without transformations"""
+        img_name = os.path.join(self.root_dir,
+            str(self.csv.iloc[idx, self.col_path_idx]) + ".jpg")
+        image = io.imread(img_name)
+
+        return image
+
+    def get_label(self, idx: int):
+        """Return original label"""
+        return self.csv.iloc[idx, self.col_label_idx]
+
+
+    def show_sample(self, sample_size: int = 4, show: bool = True,
+                    file_name: str = None, samples_per_row: int = 2,
+                    figsize: tuple = (20, 5)):
+        """Display sample of the dataset"""
+        sample_idx = np.random.choice(range(len(self)), sample_size)
+
+        num_rows = int(np.ceil(sample_size / samples_per_row))
+
+        fig, axarr = plt.subplots(num_rows, samples_per_row, figsize=figsize)
+
+        for idx, ax in zip(sample_idx, axarr.flatten()):
+            image = self.get_image(idx)
+            label = self.get_label(idx)
+            ax.imshow(image)
+            ax.axis("off")
+            ax.set_title(label)
+
+        plt.axis("off")
+
+        if file_name:
+            plt.savefig(file_name)
+        if show:
+            plt.show()
+        else:
+            plt.close()
+
+
+
+### CUDA FUNCTIONS ###
+
+def cuda_network(network):
+    """Converts network to CUDA if available"""
+    if torch.cuda.is_available():
+        print('CUDA available: converting network to CUDA')
+        network.cuda()
+    return network
+
+def get_cuda(x):
+    """ Converts tensors to cuda, if available. """
+    if torch.cuda.is_available():
+        return x.cuda()
+
+    return x
+
+def get_numpy(x):
+    """ Get numpy array for both cuda and not. """
+    if torch.cuda.is_available():
+        return x.cpu().data.numpy()
+    return x.data.numpy()
